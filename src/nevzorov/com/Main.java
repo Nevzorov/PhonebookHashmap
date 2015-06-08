3d21fe3ab52c3ea612014d85c0ddf6332fb812eb
Nevzorov <i-nevzorov@mail.ua>
2015-06-08 16:28:03 +0300

    changes

--- src/nevzorov/com/Main.java
+++ src/nevzorov/com/Main.java
@@ -18,9 +18,7 @@ public class Main {
         // Fill HashMap, HashSet
         Scanner scaner = new Scanner(fis);
         Map<String, Card> hashmap = new HashMap<>();
-        Set<Card> hashMapSet = new HashSet<>();
-        fillbookMap(scaner, hashmap, hashMapSet);
-
+        fillbookMap(scaner, hashmap);
         //Dialog for search method
         System.out.println("Choose method of search: N - name, T - number:");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -30,24 +28,37 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        if (n.equals("T") || n.equals("t"))
-            searchByNumber(reader);
+        if (n.equals("T") || n.equals("t")){
+            int tel = 0;
+            try {
+                n = reader.readLine();
+                tel = Integer.parseInt(n);
+                searchByNumber(hashmap, tel);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         if (n.equals("N") || n.equals("n"))
             searchByKey(hashmap, reader);
 
     }
 
 
-    private static void searchByNumber(BufferedReader reader) {
-        String name = null;
-        int tel = 0;
-
-            try {
-                name = reader.readLine();
-                tel = Integer.parseInt(name);
-            } catch (IOException e) {
-                e.printStackTrace();
+    private static void searchByNumber(Map<String, Card> hashmap, int tel) {
+        for (Map.Entry<String, Card> entry : hashmap.entrySet()) {
+            if ((entry.getValue()) instanceof Card){
+                Card value = entry.getValue();
+                if(value.getMnumb() == tel){
+                    System.out.println(entry.getKey());
+                    System.out.println(value.getMnumb());
+                    System.out.println(value.getHnumb());
+                    System.out.println(value.getEmail());
+                    System.out.println(value.getSkype());
+                }
             }
+        }
+
     }
 
     private static void searchByKey(Map<String, Card> hashmap, BufferedReader reader) {
@@ -66,7 +77,7 @@ public class Main {
             }
     }
 
-    private static void fillbookMap(Scanner scaner, Map<String, Card> hashmap, Set<Card> hashMapSet) {
+    private static void fillbookMap(Scanner scaner, Map<String, Card> hashmap) {
         while (scaner.hasNextLine()) {
             int i = 0;
             String line = scaner.nextLine();
@@ -81,7 +92,6 @@ public class Main {
                 String skype = (String) jsonObject.get ("skype");
                 Card card = new Card (mnumi, hnumi, email, skype);
                 hashmap.put(name, card);
-                hashMapSet.add(card);
                 }
                 i++;
             }
